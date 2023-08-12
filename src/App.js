f785376332b8dd8549c4749d6b8a1f1488a52717
Brian Prado <brravee123@gmail.com>
2023-08-13 03:14:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import * as math from "mathjs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [displayText, setDisplayText] = useState("0");
+	const [resultDisplayed, setResultDisplayed] = useState(false);
+
+	const handleButtonClick = (value) => {
+		if (resultDisplayed) {
+			if (value === "DEL") {
+				setDisplayText("0");
+				setResultDisplayed(false);
+			} else if (value === "RESET") {
+				setDisplayText("0");
+				setResultDisplayed(false);
+			} else if (value === "=") {
+				// Do nothing if = is pressed after the result
+			} else {
+				setDisplayText(formatDisplayText(displayText + value));
+				setResultDisplayed(false);
+			}
+		} else {
+			if (value === "DEL") {
+				setDisplayText(displayText.slice(0, -1));
+			} else if (value === "RESET") {
+				setDisplayText("0");
+			} else if (value === "=") {
+				try {
+					const result = math.evaluate(displayText);
+					setDisplayText(formatDisplayText(result.toString()));
+					setResultDisplayed(true);
+				} catch (error) {
+					setDisplayText("Error");
+				}
+			} else {
+				setDisplayText(
+					formatDisplayText(displayText === "0" ? value : displayText + value)
+				);
+			}
+		}
+	};
+
+	//add a comma after every 3 digits
+	const formatDisplayText = (text) => {
+		const textArray = text.split("");
+		let formattedText = "";
+		let counter = 0;
+		for (let i = textArray.length - 1; i >= 0; i--) {
+			if (counter === 3) {
+				formattedText = "," + formattedText;
+				counter = 0;
+			}
+			formattedText = textArray[i] + formattedText;
+			counter++;
+		}
+		return formattedText;
+
+		// return text;
+	};
+
+	
+	return (
+		<div>
+			<div className="container">
+				<div className="calculator">
+					<div className="calculator-display">{displayText}</div>
+					<div className="calculator-keys">
+						{/* Buttons for digits */}
+						<button className="seven" onClick={() => handleButtonClick("7")}>
+							7
+						</button>
+						<button className="eight" onClick={() => handleButtonClick("8")}>
+							8
+						</button>
+						<button className="nine" onClick={() => handleButtonClick("9")}>
+							9
+						</button>
+
+						{/* Other buttons */}
+						<button className="del" onClick={() => handleButtonClick("DEL")}>
+							DEL
+						</button>
+						<button className="four" onClick={() => handleButtonClick("4")}>
+							4
+						</button>
+						<button className="five" onClick={() => handleButtonClick("5")}>
+							5
+						</button>
+						<button className="six" onClick={() => handleButtonClick("6")}>
+							6
+						</button>
+						<button className="plus" onClick={() => handleButtonClick("+")}>
+							+
+						</button>
+						<button className="one" onClick={() => handleButtonClick("1")}>
+							1
+						</button>
+						<button className="two" onClick={() => handleButtonClick("2")}>
+							2
+						</button>
+						<button className="three" onClick={() => handleButtonClick("3")}>
+							3
+						</button>
+						<button className="minus" onClick={() => handleButtonClick("-")}>
+							-
+						</button>
+						<button className="decimal" onClick={() => handleButtonClick(".")}>
+							.
+						</button>
+						<button className="zero" onClick={() => handleButtonClick("0")}>
+							0
+						</button>
+						<button className="divide" onClick={() => handleButtonClick("/")}>
+							/
+						</button>
+						<button className="multiply" onClick={() => handleButtonClick("*")}>
+							&times;
+						</button>
+						<button
+							className="reset"
+							onClick={() => handleButtonClick("RESET")}
+						>
+							RESET
+						</button>
+						<button className="equals" onClick={() => handleButtonClick("=")}>
+							=
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
